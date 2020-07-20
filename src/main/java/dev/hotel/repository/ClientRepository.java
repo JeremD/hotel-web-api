537f22312ede9aba8fd3852f3947e9cf8a405054
JeremYD <delpech.jeremy@hotmail.fr>
2020-07-20 23:13:08 +0200

    Spring MVC: TP 6.1 Test début

--- src/main/java/dev/hotel/repository/ClientRepository.java
+++ src/main/java/dev/hotel/repository/ClientRepository.java
@@ -3,6 +3,7 @@ package dev.hotel.repository;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.hotel.entite.Client;
@@ -11,4 +12,6 @@ import dev.hotel.entite.Client;
 public interface ClientRepository extends JpaRepository<Client, UUID> {
 
 	List<Client> findAll();
+
+	List<Client> findByNom(String nom, Pageable pageable);
 }
--- src/test/java/dev/hotel/repository/ClientRepositoryTest.java
+++ src/test/java/dev/hotel/repository/ClientRepositoryTest.java
@@ -1,12 +1,19 @@
 package dev.hotel.repository;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.data.domain.Sort;
 
 import dev.hotel.controller.ClientController;
 import dev.hotel.entite.Client;
@@ -24,8 +31,15 @@ public class ClientRepositoryTest {
 	void listerClient() throws Exception {
 		
 		// Insertion des données
-		Client client = new Client("GILBERT", "Paul");
+		Client addClient = new Client("GILBERT", "Paul");
 		
+		// Génération d'un UUID
+		addClient.setUuid(addClient.getUuid());
+	
+		// Liste des clients avec Page
+		List<Client> getClient = clientRepository.findAll(PageRequest.of(0,2,Sort.by("nom").ascending())).toList();
+
+		assertThat(getClient).extracting(Client::getNom).contains("GILBERT");
 		
 		// Test Mockito
 		mockMvc.perform(MockMvcRequestBuilders.get("/client"))
