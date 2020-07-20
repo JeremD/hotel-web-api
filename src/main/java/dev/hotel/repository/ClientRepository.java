41afdb39bd7dd3afea412109e6d1da3a3baa44be
JeremYD <delpech.jeremy@hotmail.fr>
2020-07-20 16:42:23 +0200

    Spring MVC: TP 6.1 début

--- src/main/java/dev/hotel/repository/ClientRepository.java
+++ src/main/java/dev/hotel/repository/ClientRepository.java
@@ -7,8 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import dev.hotel.entite.Client;
 
-// UUID généré automatiquement
-public interface ClientRepository extends JpaRepository <Client, UUID> {
+// UUID généré depuis l'entitée Client
+public interface ClientRepository extends JpaRepository<Client, UUID> {
 
 	List<Client> findAll();
 }
