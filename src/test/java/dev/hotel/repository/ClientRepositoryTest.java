cb257dfdd8c0ca71c9d08b38d15a5724ea6f4242
JeremYD <delpech.jeremy@hotmail.fr>
2020-07-21 11:56:51 +0200

    Spring MVC: TP 6.1 (avec correction)

--- src/test/java/dev/hotel/repository/ClientRepositoryTest.java
+++ src/test/java/dev/hotel/repository/ClientRepositoryTest.java
@@ -41,10 +41,14 @@ public class ClientRepositoryTest {
 		// Ajout de client
 		Client addClient = new Client("GILBERT", "Paul");
 		addClient.setUuid(UUID.fromString("9e9c730d-a357-4dd0-b027-c8e1b079664d"));
+		
+		Client addClient2 = new Client("CLEM", "Tom");
+		addClient2.setUuid(UUID.fromString("de6df01a-8081-432a-a1ee-59687dc31e46"));
 
 		// Stockage des clients dans une liste
 		List<Client> listClient = new ArrayList<>();
 		listClient.add(addClient);
+		listClient.add(addClient2);
 		
 		// Création d'une page de liste de client
 		Page<Client> pageClient = new PageImpl<>(listClient);
@@ -53,8 +57,8 @@ public class ClientRepositoryTest {
 		Mockito.when(clientRepository.findAll(PageRequest.of(0, 5))).thenReturn(pageClient);
 
 		// Test Mockito (requête + réponse JSON)
-		mockMvc.perform(MockMvcRequestBuilders.get("/clients").param("start", "10").param("size", "10"))
-				.andExpect(MockMvcResultMatchers.status().isOk())
+		mockMvc.perform(MockMvcRequestBuilders.get("/clients").param("start", "0").param("size", "5"))
+				.andExpect(MockMvcResultMatchers.status().is2xxSuccessful())
 				.andExpect(MockMvcResultMatchers.jsonPath("$[0].uuid").isNotEmpty())
 				.andExpect(MockMvcResultMatchers.jsonPath("$[0].uuid").value("9e9c730d-a357-4dd0-b027-c8e1b079664d"))
 				.andExpect(MockMvcResultMatchers.jsonPath("$[0].nom").value("GILBERT"))
